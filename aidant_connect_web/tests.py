59aa6a783ece4c53df5fe9bd978ce3c7c51e5e58
Anna-Livia <annalivia.gomart@gmail.com>
2019-04-01 16:37:19 +0200

    Test the connexion view exists

--- aidant_connect_web/tests.py
+++ aidant_connect_web/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from aidant_connect_web.views import connection
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, connection)
