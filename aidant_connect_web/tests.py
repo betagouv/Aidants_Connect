1e871e4f6380f982e4bc84979f075fb6d9f47ed5
Anna-Livia <annalivia.gomart@gmail.com>
2019-04-01 17:22:11 +0200

    Test link to first template

--- aidant_connect_web/tests.py
+++ aidant_connect_web/tests.py
@@ -5,6 +5,10 @@ from aidant_connect_web.views import connection
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_root_url_resolves_to_connection_view(self):
         found = resolve('/')
         self.assertEqual(found.func, connection)
+
+    def test_root_url_returns_connection_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'aidant_connect_web/connection.html')
