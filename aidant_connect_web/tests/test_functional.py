f01951501d97d620545f5a69c3ec3da63af75c5a
Anna-Livia <annalivia.gomart@gmail.com>
2019-04-24 10:40:05 +0200

    Migrate functional tests to test directory

---
+++ aidant_connect_web/tests/test_functional.py
@@ -0,0 +1,31 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from django.contrib.auth.models import User
+
+
+class LoginPage(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.user = User.objects.create_user(
+            "Thierry", "thierry@thierry.com", "motdepassedethierry"
+        )
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+        cls.selenium.get('%s%s' % (cls.live_server_url, '/authorize?state=34'))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_page_loads(self):
+        login_field = self.selenium.find_element_by_id("id_username")
+        login_field.send_keys("Thierry")
+        password_field = self.selenium.find_element_by_id("id_password")
+        password_field.send_keys("motdepassedethierry")
+        submit_button = self.selenium.find_element_by_xpath('//input[@value="login"]')
+        submit_button.click()
+        welcome_aidant = self.selenium.find_element_by_id("welcome_aidant").text
+        self.assertEqual(welcome_aidant, "Bienvenue, aidant")
+        # button = self.browser.find_element_by_id("submit")
