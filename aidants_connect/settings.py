703a37a1b23083e06210996210aac37156355b51
Anna-Livia <anna-livia.gomart@beta.gouv.fr>
2019-05-07 16:03:04 +0200

    Set language and timezone

--- aidants_connect/settings.py
+++ aidants_connect/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr-fr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Paris"
 
 USE_I18N = True
 
