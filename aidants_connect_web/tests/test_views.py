322ddb8171afc4209f64340d18e624f8da2bf9d4
Anna-Livia <anna-livia.gomart@beta.gouv.fr>
2019-05-09 16:57:40 +0200

    Add test for the token route

--- aidants_connect_web/tests/test_views.py
+++ aidants_connect_web/tests/test_views.py
@@ -1,11 +1,14 @@
 import os
+import json
 from secrets import token_urlsafe
+from unittest.mock import patch
 
 from django.test.client import Client
 from django.test import TestCase
 from django.urls import resolve
 
-from aidants_connect_web.views import authorize, token, home_page
+from aidants_connect_web.views import home_page, authorize, token, user_info
+
 from aidants_connect_web.models import Connection, User
 
 fc_callback_url = os.getenv("FC_CALLBACK_URL")
@@ -63,8 +66,8 @@ class AuthorizeTests(TestCase):
 
     def test_authorize_url_triggers_the_authorize_template(self):
         self.client.login(username="Thierry", password="motdepassedethierry")
-        fc_call_state = token_urlsafe(64)
-        fc_call_nounce = token_urlsafe(64)
+        fc_call_state = token_urlsafe(4)
+        fc_call_nonce = token_urlsafe(4)
         fc_response_type = "code"
         fc_client_id = "FranceConnectInteg"
         fc_redirect_uri = (
@@ -97,15 +100,65 @@ class AuthorizeTests(TestCase):
         self.assertEqual(saved_items.count(), 1)
         code = saved_items[0].code
         state = saved_items[0].state
-        self.assertNotEqual(saved_items[0].nonce, 'No Nonce Provided')
+        self.assertNotEqual(saved_items[0].nonce, "No Nonce Provided")
         url = f"{fc_callback_url}?code={code}&state={state}"
         self.assertRedirects(response, url, fetch_redirect_response=False)
 
 
 class TokenTests(TestCase):
+    def setUp(self):
+        self.connection = Connection()
+        self.connection.state = "test_state"
+        self.connection.code = "test_code"
+        self.connection.nonce = "test_nonce"
+        self.connection.save()
+
     def test_token_url_triggers_token_view(self):
         found = resolve("/token/")
         self.assertEqual(found.func, token)
+
+    def test_token_should_respond_when_given_correct_info(self):
+        with patch.dict(
+            "os.environ",
+            {
+                "FC_AS_FS_ID": "test_client_id",
+                "FC_AS_FS_SECRET": "test_client_secret",
+                "FC_CALLBACK_URL": "test_url.test_url",
+                "HOST": "localhost",
+            },
+        ):
+            response = self.client.post(
+                "/token/",
+                {
+                    "grant_type": "authorization_code",
+                    "redirect_uri": "test_url.test_url/oidc_callback",
+                    "client_id": "test_client_id",
+                    "client_secret": "test_client_secret",
+                    "code": "test_code",
+                },
+            )
+            response_content = response.content.decode("utf-8")
+            self.assertEqual(response.status_code, 200)
+            response_json = json.loads(response_content)
+            awaited_response = {
+                "access_token": "N5ro73Y2UBpVYLc8xB137A",
+                "expires_in": 3600,
+                "id_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ0ZXN0X2Nsa"
+                "WVudF9pZCIsImV4cCI6MTMyNjUxMTg5NCwiaWF0IjoxMzI2NTExMjk0LCJ"
+                "pc3MiOiJsb2NhbGhvc3QiLCJzdWIiOiI0MzQ0MzQzNDIzIiwibm9uY2UiO"
+                "iJ0ZXN0X25vbmNlIn0.NWhma6Egbxn34v1RVtAd2wQbkCJjAIN0qyNgdKQ"
+                "qROA",
+                "refresh_token": "5ieq7Bg173y99tT6MA",
+                "token_type": "Bearer",
+            }
+
+            self.assertEqual(response_json, awaited_response)
+
+
+class UserInfoTests(TestCase):
+    def test_token_url_triggers_token_view(self):
+        found = resolve("/userinfo/")
+        self.assertEqual(found.func, user_info)
 
 
 class EnvironmentVariableTest(TestCase):
