8e15bc46f301887abf488ac6d87e1622144a0ce1
Anna-Livia Gomart <anna-livia.gomart@beta.gouv.fr>
2019-05-14 17:05:08 +0200

    Create Usager object

--- aidants_connect_web/models.py
+++ aidants_connect_web/models.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 def default_expiration_date():
@@ -18,3 +19,22 @@ class Connection(models.Model):
 
 class User(AbstractUser):
     pass
+
+
+class Usager(models.Model):
+    given_name = models.TextField(blank=False)
+    family_name = models.TextField(blank=False)
+    preferred_username = models.TextField(blank=True)
+    birthdate = models.DateField(blank=False)
+    GENDER = (("F", "Femme"), ("H", "Homme"))
+    gender = models.CharField(max_length=1, choices=GENDER, default="F", blank=False)
+    birthplace = models.PositiveIntegerField(
+        validators=[MinValueValidator(9999), MaxValueValidator(100000)], blank=False
+    )
+    birthcountry = models.IntegerField(
+        validators=[MinValueValidator(99100), MaxValueValidator(99500)],
+        default=99100,
+        blank=False,
+    )
+    sub = models.TextField(default="No Sub yet")
+    email = models.EmailField(blank=False)
--- aidants_connect_web/tests/test_models.py
+++ aidants_connect_web/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from aidants_connect_web.models import Connection
+from aidants_connect_web.models import Connection, Usager
+from datetime import date
 
 
 class ConnectionModelTest(TestCase):
@@ -24,3 +25,37 @@ class ConnectionModelTest(TestCase):
         self.assertEqual(first_saved_item.nonce, "varg")
         self.assertEqual(second_saved_item.state, "QsDfG")
 
+
+class UsagerModelTest(TestCase):
+    def test_saving_and_retrieving_usager(self):
+        first_usager = Usager()
+        first_usager.given_name = "TEST NAME"
+        first_usager.family_name = "TEST Family Name éèà"
+        first_usager.preferred_username = "I prefer being called this"
+        first_usager.birthdate = date(1902, 6, 30)
+        first_usager.gender = "F"
+        first_usager.birthplace = 27681
+        first_usager.birthcountry = 99100
+        first_usager.email = "user@test.user"
+        first_usager.save()
+
+        second_usager = Usager()
+        second_usager.given_name = "TEST SECOND NAME"
+        second_usager.family_name = "TEST Family Name éèà"
+        second_usager.preferred_username = "I prefer being called this"
+        second_usager.birthdate = date(1945, 10, 20)
+        second_usager.gender = "M"
+        second_usager.birthplace = 84016
+        second_usager.birthcountry = 99100
+        second_usager.email = "other_user@test.user"
+        second_usager.save()
+
+        saved_items = Usager.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.given_name, "TEST NAME")
+        self.assertEqual(str(first_saved_item.birthdate), "1902-06-30")
+        self.assertEqual(second_saved_item.family_name, "TEST Family Name éèà")
