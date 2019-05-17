5d362487775adcd551a8fc5345178ba2b9c23b7f
thimy <thimy@codeursenliberte.fr>
2019-05-17 14:36:59 +0200

    add custom page for 404

--- aidants_connect_web/tests/test_functional.py
+++ aidants_connect_web/tests/test_functional.py
@@ -47,3 +47,23 @@ class LoginPage(StaticLiveServerTestCase):
         welcome_aidant = self.selenium.find_element_by_id("welcome_aidant").text
         self.assertEqual(welcome_aidant, "Bienvenue, aidant")
         # button = self.browser.find_element_by_id("submit")
+
+
+class Error404Page(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+        cls.selenium.get(f"{cls.live_server_url}/thiswontwork")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_404_page(self):
+        H1 = self.selenium.find_element_by_tag_name("h1")
+        self.assertEqual(H1.text, "Cette page n’existe pas (404)")
+        link_to_home = self.selenium.find_element_by_id("to-home")
+        self.assertEqual(link_to_home.text, "Retourner à l’accueil")
