566f5029b3807d060349994266feb812fd0fe209
Anna-Livia Gomart <anna-livia.gomart@beta.gouv.fr>
2019-08-01 16:57:15 +0200

    Update front

--- aidants_connect_web/tests/test_functional/test_create_mandat.py
+++ aidants_connect_web/tests/test_functional/test_create_mandat.py
@@ -44,19 +44,22 @@ class CreateNewMandat(StaticLiveServerTestCase):
         # Create new mandat
         add_user_button = self.selenium.find_element_by_id("add_user")
         add_user_button.click()
-        procedure_section = self.selenium.find_element_by_id("select_procedure")
-        procedure_title = procedure_section.find_element_by_tag_name("h2").text
-        self.assertEqual(procedure_title, "Choisir la démarche")
+        demarches_section = self.selenium.find_element_by_id("demarches")
+        demarche_title = demarches_section.find_element_by_tag_name("h2").text
+        self.assertEqual(demarche_title, "Étape 1 : Sélectionnez la ou les démarche(s)")
 
-        procedure_list = procedure_section.find_element_by_id("id_perimeter")
-        procedures = procedure_list.find_elements_by_tag_name("label")
+        demarches_section.find_element_by_id("argent").find_element_by_tag_name(
+            "label").click()
+        demarches_section.find_element_by_id("famille").find_element_by_tag_name(
+            "label").click()
 
-        procedures[0].click()
-        procedures[-1].click()
-        self.assertEqual(len(procedures), 10)
+        demarches_grid = self.selenium.find_element_by_id("demarches_list")
+        demarches = demarches_grid.find_elements_by_tag_name("input")
+        self.assertEqual(len(demarches), 10)
 
-        duration = procedure_section.find_element_by_id("id_duration")
-        duration.send_keys("6")
+        duration_section = self.selenium.find_element_by_id("duration")
+        duration_section.find_element_by_id("long").find_element_by_tag_name(
+            "label").click()
 
         # FranceConnect
         fc_button = self.selenium.find_element_by_id("submit_button")
