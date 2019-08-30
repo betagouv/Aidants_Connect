f42f6debb082ce0c8c0904e97e55c39aa7720b4a
Anna-Livia Gomart <anna-livia.gomart@beta.gouv.fr>
2019-08-30 18:21:14 +0200

    Add migration

--- aidants_connect_web/migrations/0004_one_mandat_per_demarche.py
+++ aidants_connect_web/migrations/0004_one_mandat_per_demarche.py
@@ -6,31 +6,36 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    replaces = [('aidants_connect_web', '0004_journal_mandat_id'), ('aidants_connect_web', '0005_auto_20190827_1528'), ('aidants_connect_web', '0006_connection_mandat'), ('aidants_connect_web', '0007_auto_20190827_1630')]
-
-    dependencies = [
-        ('aidants_connect_web', '0003_auto_20190827_1432'),
+    replaces = [
+        ("aidants_connect_web", "0004_journal_mandat_id"),
+        ("aidants_connect_web", "0005_auto_20190827_1528"),
+        ("aidants_connect_web", "0006_connection_mandat"),
+        ("aidants_connect_web", "0007_auto_20190827_1630"),
     ]
+
+    dependencies = [("aidants_connect_web", "0003_auto_20190827_1432")]
 
     operations = [
         migrations.AddField(
-            model_name='journal',
-            name='mandat',
+            model_name="journal",
+            name="mandat",
             field=models.IntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='connection',
-            name='mandat',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='aidants_connect_web.Mandat'),
+            model_name="connection",
+            name="mandat",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="aidants_connect_web.Mandat",
+            ),
         ),
-        migrations.RemoveField(
-            model_name='mandat',
-            name='perimeter',
-        ),
+        migrations.RemoveField(model_name="mandat", name="perimeter"),
         migrations.AddField(
-            model_name='mandat',
-            name='demarche',
-            field=models.CharField(default='No démarche', max_length=100),
+            model_name="mandat",
+            name="demarche",
+            field=models.CharField(default="No démarche", max_length=100),
             preserve_default=False,
         ),
     ]
