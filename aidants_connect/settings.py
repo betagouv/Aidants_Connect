886424d6047c6ab705c5426faa344ac85c903487
Anna-Livia <annalivia.gomart@gmail.com>
2019-10-18 15:08:12 +0200

    Set Content security policy

--- aidants_connect/settings.py
+++ aidants_connect/settings.py
@@ -76,6 +76,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.contrib.sites.middleware.CurrentSiteMiddleware",
+    "django_referrer_policy.middleware.ReferrerPolicyMiddleware",
+    "csp.middleware.CSPMiddleware",
 ]
 
 ROOT_URLCONF = "aidants_connect.urls"
@@ -259,3 +261,20 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", None)
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", None)
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", None)
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", None)
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+REFERRER_POLICY = "strict-origin"
+
+# Content security policy
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_IMG_SRC = (
+    "'self'",
+    "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png/",
+    "https://societenumerique.gouv.fr/wp-content/uploads/2018/05/mockupkit-1.png",
+)
+CSP_SCRIPT_SRC = ("'self'", "'sha256-dzE1fiHF13yOIlSQf8CYbmucPoYAOHwQ70Y3OO70o+E='")
+CSP_STYLE_SRC = ("'self'",)
+CSP_OBJECT_SRC = ("'none'",)
+
