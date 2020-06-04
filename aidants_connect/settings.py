78d5b222a0c05edb177acb6ae320c70aa24d4d28
Raphael Odini <raphael.odini@protonmail.com>
2020-06-04 10:28:08 +0200

    Mandat perimetres: add service examples

--- aidants_connect/settings.py
+++ aidants_connect/settings.py
@@ -195,35 +195,41 @@ DEMARCHES = {
     "papiers": {
         "titre": "Papiers - Citoyenneté",
         "description": "État-civil, Passeport, Élections, Papiers à conserver, Carte d'identité…",
+        "service_exemples": ["ANTS", "Défenseur des droits"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png/papiers.png",
     },
     "famille": {
         "titre": "Famille",
         "description": "Allocations familiales, Naissance, Mariage, Pacs, Scolarité…",
+        "service_exemples": ["CAF", "ameli.fr", "EduConnect"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/famille.png",
     },
     "social": {
         "titre": "Social - Santé",
         "description": "Carte vitale, Chômage, Handicap, RSA, Personnes âgées…",
+        "service_exemples": ["ameli.fr", "MSA", "RSI"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/sante.png",
     },
     "travail": {
         "titre": "Travail",
         "description": "CDD, Concours, Retraite, Démission, Période d'essai…",
+        "service_exemples": ["Pôle emploi", "Mon compte formation", "info-retraite.fr"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/travail.png",
     },
     "logement": {
         "titre": "Logement",
         "description": "Allocations logement, Permis de construire, Logement social, Fin de bail…",
+        "service_exemples": ["CAF", "Enedis"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/logement.png",
     },
     "transports": {
         "titre": "Transports",
         "description": "Carte grise, Permis de conduire, Contrôle technique, Infractions…",
+        "service_exemples": ["ANTS", "ANTAI", "Crit'air"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/transports.png",
     },
@@ -231,24 +237,28 @@ DEMARCHES = {
         "titre": "Argent",
         "description": "Crédit immobilier, Impôts, Consommation, Livret A, Assurance, "
         "Surendettement…",
+        "service_exemples": ["Impots.gouv", "Timbres fiscaux", "Banque"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/argent.png",
     },
     "justice": {
         "titre": "Justice",
         "description": "Casier judiciaire, Plainte, Aide juridictionnelle, Saisie…",
+        "service_exemples": ["Télérecours citoyens"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/justice.png",
     },
     "etranger": {
         "titre": "Étranger",
         "description": "Titres de séjour, Attestation d’accueil, Regroupement familial…",
+        "service_exemples": ["OFPRA"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/etrangers.png",
     },
     "loisirs": {
         "titre": "Loisirs",
-        "description": "Animaux, Permis bateau, Tourisme, Permis de chasser,",
+        "description": "Animaux, Permis bateau, Tourisme, Permis de chasser…",
+        "service_exemples": ["Ariane"],
         "icon": "https://www.service-public.fr/resources/v-5cf79a7acf/web/css/img/png"
         "/loisirs.png",
     },
