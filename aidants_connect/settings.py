0f7aa1e7a74c0e663767b2ae21a3ff030f57cda8
Agnès Haasser <agnes.haasser@gmail.com>
2021-02-23 14:57:20 +0100

    Change wait template of magicauth

--- aidants_connect/settings.py
+++ aidants_connect/settings.py
@@ -283,6 +283,7 @@ MAGICAUTH_LOGIN_VIEW_TEMPLATE = "login/login.html"
 MAGICAUTH_EMAIL_SENT_VIEW_TEMPLATE = "login/email_sent.html"
 MAGICAUTH_EMAIL_HTML_TEMPLATE = "login/email_template.html"
 MAGICAUTH_EMAIL_TEXT_TEMPLATE = "login/email_template.txt"
+MAGICAUTH_WAIT_VIEW_TEMPLATE = "login/wait.html"
 MAGICAUTH_ENABLE_2FA = True
 
 # https://github.com/betagouv/django-magicauth/blob/8a8143388bb15fad2823528201e22a31817da243/magicauth/settings.py#L54  # noqa
