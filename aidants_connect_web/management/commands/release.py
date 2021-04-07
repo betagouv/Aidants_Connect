74df37bd81bbaff392ca07b380b97e83e71e97a7
Agnès Haasser <agnes@corelka.fr>
2021-04-07 18:12:08 +0200

    Split the main method into smaller ones

--- aidants_connect_web/management/commands/release.py
+++ aidants_connect_web/management/commands/release.py
@@ -1,4 +1,5 @@
 import os
+
 from django.core.management.base import BaseCommand
 
 
@@ -6,22 +7,32 @@ class Command(BaseCommand):
     help = "Generate a new tag using provided version number"
 
     def handle(self, *args, **options):
-        self.stdout.write("Here are the latest versions:")
-        os.system("git tag -l | tail")
-        version = input("Which version number will you give to the new version? ")
-        are_they_sure = input(
-            self.style.WARNING(f"Version {version}, are you sure? (y/N) ")
-        )
-        if are_they_sure.lower().strip() != "y":
+        self.version = self.ask_for_new_version()
+
+        if not self.confirm_if_ok():
             self.stdout.write(
                 "You’re right, there’s no rush. Try again when you are ready."
             )
             return
         self.stdout.write("OK, let’s tag this then!")
 
+        self.tag_and_push_on_origin(self.version)
+
+    def ask_for_new_version(self):
+        self.stdout.write("Here are the latest versions:")
+        os.system("git tag -l | tail")
+        version = input("Which version number will you give to the new version? ")
+        return version
+
+    def confirm_if_ok(self):
+        are_they_sure = input(
+            self.style.WARNING(f"Version {self.version}, are you sure? (y/N) ")
+        )
+        return are_they_sure.lower().strip() == "y"
+
+    def tag_and_push_on_origin(self, version):
         os.system(f'git tag -a {version} -m ""')
         os.system("git push --tags")
-
         self.stdout.write(
             self.style.SUCCESS(f"Tag {version} was pushed to git origin!")
         )
