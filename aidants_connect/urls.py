af039dda916c121058e0e977cebc04068d0ddec2
Christophe Henry <contact2@c-henry.fr>
2021-06-10 14:52:04 +0200

    Fix 404 on /favicon.ico

--- aidants_connect/urls.py
+++ aidants_connect/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.urls import path, include
 
+from aidants_connect import views
 from aidants_connect_web.admin import admin_site
 
 urlpatterns = [
+    path("favicon.ico", views.favicon),
     path(settings.ADMIN_URL, admin_site.urls),
     path("admin/", include("admin_honeypot.urls", namespace="admin_honeypot")),
     path("", include("aidants_connect_web.urls")),
