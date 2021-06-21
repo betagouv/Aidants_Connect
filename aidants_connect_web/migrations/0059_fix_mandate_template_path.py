80356b3aad4830443038d524db2c25f9400278b8
Christophe Henry <contact2@c-henry.fr>
2021-06-21 16:17:19 +0200

    Prevent modify template path for madates that are older than 2021-03-08

--- aidants_connect_web/migrations/0059_fix_mandate_template_path.py
+++ aidants_connect_web/migrations/0059_fix_mandate_template_path.py
@@ -1,10 +1,11 @@
-from datetime import timedelta
-from os import walk as os_walk
-from os.path import dirname, join as path_join
+from datetime import timedelta, datetime
+from glob import glob
+from os.path import dirname, basename, join as path_join
 
 from django.db import migrations, transaction
 from django.template import loader
 from django.db.models import Q
+from django.utils.timezone import get_default_timezone
 
 from aidants_connect import settings
 from aidants_connect_web.utilities import generate_attestation_hash
@@ -18,6 +19,10 @@ def populate_template_path(apps, _):
     Mandat = apps.get_model("aidants_connect_web", "Mandat")
 
     for mandate in Mandat.objects.all().order_by("creation_date"):
+        # Prevent erasing the field for mandate that are newer than 8th of march, 2021
+        if mandate.creation_date >= datetime(2021, 3, 8, tzinfo=get_default_timezone()):
+            continue
+
         journal = None
 
         journal_qset = Journal.objects.filter(
@@ -54,29 +59,34 @@ def populate_template_path(apps, _):
             loader.get_template(settings.MANDAT_TEMPLATE_PATH).origin.name
         )
 
-        for _, _, filenames in os_walk(template_dir):
-            for filename in filenames:
-                file_hash = generate_attestation_hash(
-                    journal.aidant,
-                    mandate.usager,
-                    [it.demarche for it in mandate.autorisations.all()],
-                    mandate.expiration_date,
-                    journal.creation_date.date().isoformat(),
-                    path_join(settings.MANDAT_TEMPLATE_DIR, filename),
-                )
+        files = glob(f"{template_dir}/**/*.html", recursive=True)
+        for file in files:
+            filename = basename(file)
+            file_hash = generate_attestation_hash(
+                journal.aidant,
+                mandate.usager,
+                [it.demarche for it in mandate.autorisations.all()],
+                mandate.expiration_date,
+                journal.creation_date.date().isoformat(),
+                path_join(settings.MANDAT_TEMPLATE_DIR, filename),
+            )
 
-                if file_hash == journal.attestation_hash:
-                    mandate.template_path = path_join(
-                        settings.MANDAT_TEMPLATE_DIR, filename
-                    )
-                    mandate.save()
+            if file_hash == journal.attestation_hash:
+                mandate.template_path = path_join(
+                    settings.MANDAT_TEMPLATE_DIR, filename
+                )
+                mandate.save()
+                break
+        else:
+            mandate.template_path = ""
+            mandate.save()
 
 
 class Migration(migrations.Migration):
     atomic = False
 
     dependencies = [
-        ("aidants_connect_web", "0057_auto_20210525_1559"),
+        ("aidants_connect_web", "0058_aidant_validated_cgu_version"),
     ]
 
     operations = [
