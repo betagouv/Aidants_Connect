ff85f72332e47315955b3a03aa100a48686e1918
Agnès Haasser <agnes@corelka.fr>
2022-01-12 10:55:33 +0100

    Display organisationrequests and issuers in admin

--- aidants_connect_habilitation/admin.py
+++ aidants_connect_habilitation/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from aidants_connect.admin import admin_site, VisibleToAdminMetier
+from aidants_connect_habilitation.models import Issuer, OrganisationRequest
+
+
+class IssuerAdmin(VisibleToAdminMetier, ModelAdmin):
+    list_display = (
+        "email",
+        "last_name",
+        "first_name",
+        "phone",
+        "id",
+    )
+
+
+class OrganisationRequestAdmin(VisibleToAdminMetier, ModelAdmin):
+    raw_id_fields = ("issuer",)
+
+
+admin_site.register(Issuer, IssuerAdmin)
+admin_site.register(OrganisationRequest, OrganisationRequestAdmin)
