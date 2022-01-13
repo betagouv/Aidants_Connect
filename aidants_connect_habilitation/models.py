51b438630e5c3137be6dc71ec5be1cd673d42584
Agnès Haasser <agnes@corelka.fr>
2022-01-13 17:20:07 +0100

    Adjust model to improve form display

--- aidants_connect_habilitation/models.py
+++ aidants_connect_habilitation/models.py
@@ -47,12 +47,13 @@ class OrganisationRequest(models.Model):
     type_other = models.CharField(
         "Type de structure si autre",
         null=True,
+        blank=True,
         default=None,
         max_length=200,
     )
 
     # Organisation
-    name = models.TextField("Nom", default="No name provided")
+    name = models.TextField("Nom de la structure", default="No name provided")
     siret = models.BigIntegerField("N° SIRET", default=1)
     address = models.TextField("Adresse", default="No address provided")
     zipcode = models.CharField("Code Postal", max_length=10, default="0")
@@ -66,7 +67,10 @@ class OrganisationRequest(models.Model):
     )
 
     public_service_delegation_attestation = models.FileField(
-        "Attestation de délégation de service public", null=True, default=None
+        "Attestation de délégation de service public",
+        null=True,
+        default=None,
+        blank=True,
     )
 
     france_services_label = models.BooleanField(
@@ -82,13 +86,13 @@ class OrganisationRequest(models.Model):
     )
 
     # Manager
-    manager_first_name = models.CharField("Prénom", max_length=150)
-    manager_last_name = models.CharField("Nom", max_length=150)
-    manager_email = models.EmailField(max_length=150)
+    manager_first_name = models.CharField("Prénom du responsable", max_length=150)
+    manager_last_name = models.CharField("Nom du responsable", max_length=150)
+    manager_email = models.EmailField("Email du responsable", max_length=150)
     manager_profession = models.CharField(
-        "Intitulé du poste", blank=False, max_length=150
+        "Profession du responsable", blank=False, max_length=150
     )
-    manager_phone = PhoneNumberField(blank=True)
+    manager_phone = PhoneNumberField("Téléphone du responsable", blank=True)
 
     # Checkboxes
     cgu = models.BooleanField("J'accepte les CGU")
@@ -97,6 +101,9 @@ class OrganisationRequest(models.Model):
         "La structure ne contient que des aidants professionnels"
     )
     without_elected = models.BooleanField("Aucun élu n'est impliqué dans la structure")
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         constraints = [
@@ -138,7 +145,13 @@ class AidantRequest(models.Model):
     last_name = models.CharField("Nom", max_length=150)
     email = models.EmailField(max_length=150)
     profession = models.CharField("Intitulé du poste", blank=False, max_length=150)
-    phone = PhoneNumberField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "aidant à habiliter"
+        verbose_name_plural = "aidants à habiliter"
 
 
 class RequestMessage(models.Model):
