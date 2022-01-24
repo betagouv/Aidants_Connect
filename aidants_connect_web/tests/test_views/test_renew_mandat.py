6282c9917668b803bcc65c9457bb17d08f2b6018
Agnès Haasser <agnes@corelka.fr>
2022-01-24 14:39:27 +0100

    Add on test on nominal test case

--- aidants_connect_web/tests/test_views/test_renew_mandat.py
+++ aidants_connect_web/tests/test_views/test_renew_mandat.py
@@ -9,6 +9,7 @@ from aidants_connect_web.models import Connection, Mandat, Journal
 from aidants_connect_web.tests.factories import (
     AidantFactory,
     MandatFactory,
+    RevokedMandatFactory,
     OrganisationFactory,
     UsagerFactory,
 )
@@ -26,6 +27,22 @@ class RenewMandatTests(TestCase):
         device.token_set.create(token="223456")
 
         cls.usager = UsagerFactory(given_name="Fabrice")
+
+    def test_no_renew_button_displayed_if_should_not(self):
+        mandat = RevokedMandatFactory(
+            organisation=self.aidant_thierry.organisation,
+            usager=self.usager,
+            expiration_date=timezone.now() + timedelta(days=5),
+            post__create_authorisations=["argent", "famille", "logement"],
+        )
+        self.assertTrue(
+            mandat.was_explicitly_revoked,
+            "Generated mandat should be explicitly revoked",
+        )
+        self.client.force_login(self.aidant_thierry)
+        self.assertEqual(Mandat.objects.count(), 1)
+        response = self.client.get(reverse("usagers"))
+        self.assertNotContains(response, "Renouveler")
 
     def test_renew_mandat_ok(self):
         MandatFactory(
