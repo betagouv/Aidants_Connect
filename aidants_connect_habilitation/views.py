4515685a0cde0a56c4fecb17368b7a8174ea7396
Agnès Haasser <agnes@corelka.fr>
2022-03-08 09:10:57 +0100

    Add view and design for page "readonly view of request"

--- aidants_connect_habilitation/views.py
+++ aidants_connect_habilitation/views.py
@@ -297,3 +297,14 @@ class ValidationRequestFormView(LateStageRequestView, HabilitationStepMixin, For
 
     def get_success_url(self):
         return reverse("habilitation_new_issuer")
+
+
+class ReadonlyRequestView(LateStageRequestView, TemplateView):
+    template_name = "view_organisation_request.html"
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            "organisation": self.organisation,
+            "aidants": self.organisation.aidant_requests,
+        }
