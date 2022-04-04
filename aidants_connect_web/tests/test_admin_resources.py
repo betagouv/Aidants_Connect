365be1eeda3fab367d9b2965cab2bdf056cca1d3
MrJmad <j-mad@j-mad.com>
2022-04-04 08:02:41 +0200

    Export Aidant et HabilitationRequest pour la sandbox

--- aidants_connect_web/tests/test_admin_resources.py
+++ aidants_connect_web/tests/test_admin_resources.py
@@ -1,10 +1,16 @@
+from django.contrib.admin.sites import AdminSite
 from django.test import TestCase, tag
+from django.test.client import RequestFactory
 
 import tablib
 
-from aidants_connect_web.admin import OrganisationResource
+from aidants_connect_web.admin import OrganisationAdmin, OrganisationResource
 from aidants_connect_web.models import Organisation
-from aidants_connect_web.tests.factories import OrganisationFactory
+from aidants_connect_web.tests.factories import (
+    AidantFactory,
+    HabilitationRequestFactory,
+    OrganisationFactory,
+)
 
 
 @tag("admin")
@@ -169,3 +175,46 @@ class OrganisationResourceTestCase(TestCase):
         self.assertEqual(1, Organisation.objects.all().count())
         self.assertEqual(2323, Organisation.objects.first().data_pass_id)
         self.assertEqual("34034", Organisation.objects.first().zipcode)
+
+
+@tag("admin")
+class OrganisationResourceExportForSandboxTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.rf = RequestFactory()
+        cls.organisation1 = OrganisationFactory(name="MAIRIE", siret="121212121")
+        cls.organisation2 = OrganisationFactory(name="MAIRIE2", siret="121212122")
+        cls.organisation3 = OrganisationFactory(name="MAIRIE3", siret="121212123")
+
+        cls.aidant_marge = AidantFactory(
+            first_name="Marge", organisation=cls.organisation1
+        )
+        cls.aidant_homer = AidantFactory(
+            first_name="Homer", organisation=cls.organisation2
+        )
+        cls.habilit_bowser = HabilitationRequestFactory(
+            first_name="Bowser", organisation=cls.organisation3
+        )
+
+    def get_list_for_export_sandbox(self):
+        orgas = Organisation.objects.filter(
+            pk__in=[self.organisation1.pk, self.organisation3.pk]
+        )
+
+        export_list = OrganisationAdmin.get_list_for_export_sandbox(orgas)
+        self.assertEqual(len(export_list), 2)
+        self.assertTrue(self.aidant_marge in export_list)
+        self.assertTrue(self.habilit_bowser in export_list)
+
+    def test_export_for_sandbox(self):
+        request = self.rf.get("/", {})
+        orgas = Organisation.objects.filter(
+            pk__in=[self.organisation1.pk, self.organisation3.pk]
+        )
+        orga_admin = OrganisationAdmin(Organisation, AdminSite())
+        data = orga_admin.get_data_for_export(request, orgas)
+        self.assertEqual(len(data), 2)
+        self.assertEqual(len(data[0]), 12)
+        self.assertEqual(data[0][1], "Marge")
+        self.assertEqual(data[0][3], self.aidant_marge.email)
+        self.assertEqual(data[1][1], "Bowser")
