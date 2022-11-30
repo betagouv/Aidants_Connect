c1c9fb52f481cde7ec2c9ce5a9eb2447a57a7bad
Christophe Henry <contact@c-henry.fr>
2022-11-30 13:49:44 +0100

    Handle diacritics in search

--- aidants_connect_web/static/js/hidden_users_search.js
+++ aidants_connect_web/static/js/hidden_users_search.js
@@ -1,9 +1,35 @@
 "use strict";
 
 $(function () {
+    const collator = new Intl.Collator(navigator.language, {
+        usage: "search",
+        sensitivity: "base",
+    })
+
+    // Inspired by https://github.com/idmadj/locale-includes/blob/master/src/index.js
+    function localeIncludes(haystack, needle) {
+        const haystackLength = haystack.length
+        const needleLength = needle.length
+        const lengthDiff = haystackLength - needleLength
+
+        for (var i = 0; i <= lengthDiff; i++) {
+            const subHaystack = haystack.substring(i, i + needleLength)
+            if (collator.compare(subHaystack, needle) === 0) {
+                return true
+            }
+        }
+        return false
+    }
+
     const users = JSON.parse(document.querySelector("#usagers_list").textContent);
     $("#filter-input").autocomplete({
-        source: users,
+        source: function (request, response) {
+            const results = users.filter(function(item) {
+                return localeIncludes(item.label, request.term);
+            });
+
+            response(results);
+        },
         minLength: 3,
         focus: function (event, ui) {
             $("#filter-input").val(ui.item.label);
