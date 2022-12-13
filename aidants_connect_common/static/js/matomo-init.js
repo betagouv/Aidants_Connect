744614ff604deb59021089b7aca5c40eb51bf8db
Christophe Henry <contact@c-henry.fr>
2022-12-13 16:01:05 +0100

    Fix Matomo script (again)

--- aidants_connect_common/static/js/matomo-init.js
+++ aidants_connect_common/static/js/matomo-init.js
@@ -1,16 +1,16 @@
 (function () {
-    const matomoUrl = document.querySelector("#matomo-script").dataset.matomoUrl;
-    const matomoSiteId = document.querySelector("#matomo-script").dataset.matomoSiteId;
+    const el = document.querySelector("#matomo-script");
+    const matomoUrl = el.dataset.matomoUrl.replace(/\/+$/, "");
+    const matomoSiteId = el.dataset.matomoSiteId;
     var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(["trackPageView"]);
     _paq.push(["enableLinkTracking"]);
     (function () {
-        _paq.push(["setTrackerUrl", matomoUrl + "matomo.php"]);
+        _paq.push(["setTrackerUrl", matomoUrl + "/matomo.php"]);
         _paq.push(["setSiteId", matomoSiteId]);
-        var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
-        g.async = true;
-        g.src = "https://cdn.matomo.cloud/gouv.matomo.cloud/matomo.js";
-        s.parentNode.insertBefore(g, s);
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = matomoUrl + "/matomo.js";
+        document.querySelector("body").insertAdjacentElement("beforeend", script);
     })();
 })();
