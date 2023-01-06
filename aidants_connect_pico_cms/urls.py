efffc353b7da8dcd5b3716fefe0cc65a66864b57
Agnès Haasser <agnes@corelka.fr>
2023-01-06 12:36:40 +0100

    Add view and template for FAQ

--- aidants_connect_pico_cms/urls.py
+++ aidants_connect_pico_cms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from aidants_connect_pico_cms.views import TestimonyView
+from aidants_connect_pico_cms.views import FaqCategoryView, TestimonyView
 
 urlpatterns = [
     path("temoignages/<slug:slug>/", TestimonyView.as_view(), name="temoignage-detail"),
+    path("faq/<slug:slug>/", FaqCategoryView.as_view(), name="faq-category-detail"),
 ]
--- aidants_connect_pico_cms/views.py
+++ aidants_connect_pico_cms/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.detail import DetailView
 
-from aidants_connect_pico_cms.models import Testimony
+from aidants_connect_pico_cms.models import FaqCategory, FaqQuestion, Testimony
 
 
 class TestimonyView(DetailView):
@@ -11,4 +11,18 @@ class TestimonyView(DetailView):
         context["all_testimonies"] = Testimony.objects.filter(published=True).order_by(
             "sort_order"
         )
+        return context
+
+
+class FaqCategoryView(DetailView):
+    model = FaqCategory
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = FaqCategory.objects.filter(published=True).order_by(
+            "sort_order"
+        )
+        context["questions"] = FaqQuestion.objects.filter(
+            published=True, category=self.object
+        ).order_by("sort_order")
         return context
