94de9af2770a656f61a73b2decfd5050a528de97
Christophe Henry <contact@c-henry.fr>
2023-02-09 14:14:12 +0100

    Handle period in SMS response

--- aidants_connect_web/tests/test_views/test_sms.py
+++ aidants_connect_web/tests/test_views/test_sms.py
@@ -9,3 +9,12 @@ class CallbackTests(TestCase):
     def test_sms_callback_url_triggers_the_correct_view(self):
         found = resolve(reverse("sms_callback"))
         self.assertEqual(found.func.view_class, sms.Callback)
+
+    def test__check_user_consents(self):
+        target = sms.Callback()
+        self.assertTrue(target._check_user_consents("Oui"))
+        self.assertTrue(target._check_user_consents("  Oui  "))
+        self.assertTrue(target._check_user_consents("  Oui  ."))
+        self.assertTrue(target._check_user_consents("  Oui  .  "))
+        self.assertFalse(target._check_user_consents("Oui;"))
+        self.assertFalse(target._check_user_consents("Oui non"))
