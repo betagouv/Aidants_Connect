0114bacd460c0523f8a92864d1071735a5320623
Christophe Henry <contact@c-henry.fr>
2023-05-09 12:46:19 +0200

    Markdown and markdown editor improvements

--- aidants_connect_pico_cms/utils.py
+++ aidants_connect_pico_cms/utils.py
@@ -2,6 +2,7 @@ from django.utils.html import escape
 
 from markdown import markdown
 from markdown.extensions.attr_list import AttrListExtension
+from markdown.extensions.nl2br import Nl2BrExtension
 
 from aidants_connect import settings
 
@@ -10,6 +11,7 @@ def render_markdown(content: str) -> str:
     return markdown(
         escape(content),
         extensions=[
+            Nl2BrExtension(),  # New line will be treated as linebreak
             AttrListExtension(),  # Allows to add HTML classes and ID
         ],
     )
