c26b740571e6e83e927347ae543e2bfb8cff8756
Christophe Henry <contact@c-henry.fr>
2023-06-05 16:37:18 +0200

    Render connection email using MJML

--- aidants_connect_web/views/login.py
+++ aidants_connect_web/views/login.py
@@ -1,7 +1,11 @@
 from magicauth import views as magicauth_views
 
+from aidants_connect_common.utils.email import render_email
 from aidants_connect_web.forms import LoginEmailForm
 
 
 class LoginView(magicauth_views.LoginView):
     form_class = LoginEmailForm
+
+    def render_email(self, context):
+        return render_email(self.html_template, context)
