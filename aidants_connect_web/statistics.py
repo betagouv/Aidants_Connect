9b54e37d04dd72b709e069af727092a0b0249d7e
MrJmad <j-mad@j-mad.com>
2023-06-07 15:53:16 +0200

    Compute new statistics (Total)

--- aidants_connect_web/statistics.py
+++ aidants_connect_web/statistics.py
@@ -1,23 +1,59 @@
 from django.conf import settings
 
-from .models import Aidant, AidantStatistiques, Journal
+# from aidants_connect_common.models import Department, Region
+from aidants_connect_common.utils.constants import (
+    JournalActionKeywords,
+    RequestStatusConstants,
+)
+from aidants_connect_habilitation.models import OrganisationRequest
+
+from .models import (
+    Aidant,
+    AidantStatistiques,
+    AidantStatistiquesbyDepartment,
+    AidantStatistiquesbyRegion,
+    HabilitationRequest,
+    Journal,
+    Organisation,
+)
 
 
-def compute_statistics():
+def compute_all_statistics():
+    pass
+
+
+def compute_statistics(
+    ostat: [
+        AidantStatistiques,
+        AidantStatistiquesbyDepartment,
+        AidantStatistiquesbyRegion,
+    ]
+):
+
     stafforg = settings.STAFF_ORGANISATION_NAME
     ads = Aidant.objects.exclude(organisation__name=stafforg)
+    orgas = Organisation.objects.exclude(name=stafforg)
+
     number_aidants = ads.count()
-    number_aidants_is_active = ads.filter(is_active=True).count()
-    number_responsable = ads.filter(is_active=True, can_create_mandats=False).count()
-    number_aidant_can_create_mandat = ads.filter(
-        is_active=True, can_create_mandats=True
-    ).count()
-    number_aidants_without_totp = ads.filter(
+    qs_aidants_is_active = ads.filter(is_active=True)
+    number_aidants_is_active = qs_aidants_is_active.count()
+
+    qs_responsable = ads.filter(is_active=True, can_create_mandats=False)
+    number_responsable = qs_responsable.count()
+
+    qs_aidant_can_create_mandat = ads.filter(is_active=True, can_create_mandats=True)
+    number_aidant_can_create_mandat = qs_aidant_can_create_mandat.count()
+
+    qs_aidants_without_totp = ads.filter(
         is_active=True, can_create_mandats=True, carte_totp__isnull=True
-    ).count()
-    number_aidant_with_login = ads.filter(
+    )
+    number_aidants_without_totp = qs_aidants_without_totp.count()
+
+    qs_aidant_with_login = ads.filter(
         is_active=True, can_create_mandats=True, last_login__isnull=False
-    ).count()
+    )
+    number_aidant_with_login = qs_aidant_with_login.count()
+
     aids_id = set(
         list(
             Journal.objects.filter(action="create_attestation").values_list(
@@ -25,18 +61,94 @@ def compute_statistics():
             )
         )
     )
-    number_aidant_who_have_created_mandat = ads.filter(
+    qs_aidant_who_have_created_mandat = ads.filter(
         is_active=True, can_create_mandats=True, pk__in=aids_id
-    ).count()
-
-    stats = AidantStatistiques(
-        number_aidants=number_aidants,
-        number_aidants_is_active=number_aidants_is_active,
-        number_responsable=number_responsable,
-        number_aidant_can_create_mandat=number_aidant_can_create_mandat,
-        number_aidants_without_totp=number_aidants_without_totp,
-        number_aidant_with_login=number_aidant_with_login,
-        number_aidant_who_have_created_mandat=number_aidant_who_have_created_mandat,
     )
-    stats.save()
-    return stats
+    number_aidant_who_have_created_mandat = qs_aidant_who_have_created_mandat.count()
+
+    qs_operational_aidants = ads.filter(
+        is_active=True, can_create_mandats=True, carte_totp__isnull=False
+    )
+    number_operational_aidants = qs_operational_aidants.count()
+
+    qs_future_aidant = HabilitationRequest.objects.exclude(
+        status__in=[
+            HabilitationRequest.STATUS_REFUSED,
+            HabilitationRequest.STATUS_CANCELLED,
+            HabilitationRequest.STATUS_VALIDATED,
+        ]
+    )
+    number_future_aidant = qs_future_aidant.count()
+
+    qs_trained_aidant_since_begining = ads.filter(can_create_mandats=True)
+    number_trained_aidant_since_begining = qs_trained_aidant_since_begining.count()
+
+    qs_future_trained_aidant = HabilitationRequest.objects.filter(
+        formation_done=True
+    ).exclude(
+        status__in=[
+            HabilitationRequest.STATUS_VALIDATED,
+        ],
+    )
+    number_future_trained_aidant = qs_future_trained_aidant.count()
+
+    nb_structures = orgas.count()
+
+    qs_orga_requests = OrganisationRequest.objects.exclude(
+        status=RequestStatusConstants.VALIDATED.name
+    )
+    nb_orga_requests = qs_orga_requests.count()
+
+    qs_validated_orga_requests = OrganisationRequest.objects.filter(
+        status=RequestStatusConstants.VALIDATED.name
+    )
+    nb_validated_orga_requests = qs_validated_orga_requests.count()
+
+    number_organisation_requests = nb_structures + nb_orga_requests
+    number_validated_organisation_requests = nb_structures + nb_validated_orga_requests
+
+    qs_organisation_with_accredited_aidants = orgas.filter(
+        aidants__in=qs_operational_aidants
+    )
+    number_organisation_with_accredited_aidants = (
+        qs_organisation_with_accredited_aidants.count()
+    )
+
+    qs_organisation_with_at_least_one_ac_usage = orgas.filter(
+        journal_entries__action__in=[
+            JournalActionKeywords.FRANCECONNECT_USAGER,
+            JournalActionKeywords.CREATE_ATTESTATION,
+            JournalActionKeywords.CREATE_AUTORISATION,
+            JournalActionKeywords.USE_AUTORISATION,
+            JournalActionKeywords.INIT_RENEW_MANDAT,
+        ]
+    ).distinct()
+
+    number_organisation_with_at_least_one_ac_usage = (
+        qs_organisation_with_at_least_one_ac_usage.count()
+    )
+
+    qs_usage_of_ac = Journal.objects.filter(
+        action__in=[JournalActionKeywords.USE_AUTORISATION]
+    )
+    number_usage_of_ac = qs_usage_of_ac.count()
+
+    ostat.number_aidants = number_aidants
+    ostat.number_aidants_is_active = number_aidants_is_active
+    ostat.number_responsable = number_responsable
+    ostat.number_aidant_can_create_mandat = number_aidant_can_create_mandat
+    ostat.number_aidants_without_totp = number_aidants_without_totp
+    ostat.number_aidant_with_login = number_aidant_with_login
+    ostat.number_aidant_who_have_created_mandat = number_aidant_who_have_created_mandat  # noqa
+    ostat.number_operational_aidants = number_operational_aidants
+    ostat.number_future_aidant = number_future_aidant
+    ostat.number_trained_aidant_since_begining = number_trained_aidant_since_begining  # noqa
+    ostat.number_future_trained_aidant = number_future_trained_aidant
+    ostat.number_organisation_requests = number_organisation_requests
+    ostat.number_validated_organisation_requests = number_validated_organisation_requests  # noqa
+    ostat.number_organisation_with_accredited_aidants = number_organisation_with_accredited_aidants  # noqa
+    ostat.number_organisation_with_at_least_one_ac_usage = number_organisation_with_at_least_one_ac_usage  # noqa
+    ostat.number_usage_of_ac = number_usage_of_ac
+    ostat.save()
+
+    return ostat
--- aidants_connect_web/tests/test_stats.py
+++ aidants_connect_web/tests/test_stats.py
@@ -2,12 +2,15 @@ from django.conf import settings
 from django.test import TestCase, tag
 from django.utils.timezone import now
 
-from aidants_connect_web.models import Aidant
+from aidants_connect_common.utils.constants import JournalActionKeywords
+from aidants_connect_web.models import Aidant, AidantStatistiques, HabilitationRequest
 from aidants_connect_web.statistics import compute_statistics
 from aidants_connect_web.tests.factories import (
     AidantFactory,
     AttestationJournalFactory,
     CarteTOTPFactory,
+    HabilitationRequestFactory,
+    JournalFactory,
     OrganisationFactory,
 )
 
@@ -19,12 +22,17 @@ class StatisticsTests(TestCase):
         super().setUpClass()
         staf_orga = OrganisationFactory(name=settings.STAFF_ORGANISATION_NAME)
         orga = OrganisationFactory(name="CCAS")
+        # nb = 0
         AidantFactory(organisation=staf_orga)
+        # nb = 0
         AidantFactory(is_active=False)
+        # nb = 1
         AidantFactory()
+        # nb = 2
         ad_with_totp = AidantFactory()
         CarteTOTPFactory(aidant=ad_with_totp)
 
+        # nb = 3
         AidantFactory(
             post__with_otp_device=True,
             organisation=orga,
@@ -33,7 +41,7 @@ class StatisticsTests(TestCase):
         )
 
     def test_one_compute_statistics(self):
-        stats = compute_statistics()
+        stats = compute_statistics(AidantStatistiques())
         self.assertEqual(Aidant.objects.count(), 5)
         self.assertEqual(stats.number_aidants, 4)
         self.assertEqual(stats.number_aidants_is_active, 3)
@@ -50,8 +58,74 @@ class StatisticsTests(TestCase):
         CarteTOTPFactory(aidant=self.ad_with_totp_three)
 
         AttestationJournalFactory(aidant=self.ad_with_totp_three)
-        stats = compute_statistics()
+        stats = compute_statistics(AidantStatistiques())
 
         self.assertEqual(stats.number_aidants, 6)
         self.assertEqual(stats.number_aidant_with_login, 2)
         self.assertEqual(stats.number_aidant_who_have_created_mandat, 1)
+
+    def test_computing_new_statistics(self):
+        orga_ad_two = OrganisationFactory(name="FService AD 2")
+        orga_ad_three = OrganisationFactory(name="FService AD 3")
+
+        orga_ad_four = OrganisationFactory(name="FService Autre")
+
+        self.ad_with_totp_two = AidantFactory(
+            last_login=now(), organisation=orga_ad_two
+        )
+        CarteTOTPFactory(aidant=self.ad_with_totp_two)
+        self.ad_with_totp_three = AidantFactory(
+            last_login=now(), organisation=orga_ad_three
+        )
+        CarteTOTPFactory(aidant=self.ad_with_totp_three)
+
+        self.ad_with_totp_four = AidantFactory(
+            last_login=now(), organisation=orga_ad_four
+        )
+        CarteTOTPFactory(aidant=self.ad_with_totp_four)
+
+        AttestationJournalFactory(
+            aidant=self.ad_with_totp_three,
+            organisation=self.ad_with_totp_three.organisation,
+        )
+
+        AttestationJournalFactory(
+            aidant=self.ad_with_totp_three,
+            organisation=self.ad_with_totp_three.organisation,
+        )
+
+        AttestationJournalFactory(
+            aidant=self.ad_with_totp_two,
+            organisation=self.ad_with_totp_two.organisation,
+        )
+
+        JournalFactory(
+            organisation=orga_ad_four,
+            aidant=self.ad_with_totp_four,
+            action=JournalActionKeywords.USE_AUTORISATION,
+        )
+
+        HabilitationRequestFactory(
+            status=HabilitationRequest.STATUS_VALIDATED, formation_done=True
+        )
+        HabilitationRequestFactory(
+            status=HabilitationRequest.STATUS_REFUSED, formation_done=True
+        )
+        HabilitationRequestFactory(
+            status=HabilitationRequest.STATUS_NEW, formation_done=True
+        )
+        HabilitationRequestFactory(
+            status=HabilitationRequest.STATUS_NEW, formation_done=True
+        )
+        HabilitationRequestFactory(status=HabilitationRequest.STATUS_NEW)
+
+        stats = compute_statistics(AidantStatistiques())
+
+        self.assertEqual(stats.number_aidant_who_have_created_mandat, 2)
+        self.assertEqual(stats.number_operational_aidants, 4)
+        self.assertEqual(stats.number_future_aidant, 3)
+        self.assertEqual(stats.number_future_trained_aidant, 3)
+        self.assertEqual(stats.number_trained_aidant_since_begining, 6)
+
+        self.assertEqual(stats.number_organisation_with_accredited_aidants, 4)
+        self.assertEqual(stats.number_organisation_with_at_least_one_ac_usage, 3)
