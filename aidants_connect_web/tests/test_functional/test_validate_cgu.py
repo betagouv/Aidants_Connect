244927aa5022de5bedefe9480da3935f493226be
Christophe Henry <contact@c-henry.fr>
2024-05-07 11:51:44 +0200

    Fix CGU form validation

---
+++ aidants_connect_web/tests/test_functional/test_validate_cgu.py
@@ -0,0 +1,45 @@
+from django.conf import settings
+from django.test import tag
+from django.urls import reverse
+
+from selenium.webdriver.common.by import By
+
+from aidants_connect_common.tests.testcases import FunctionalTestCase
+from aidants_connect_web.models import Aidant
+from aidants_connect_web.tests.factories import AidantFactory
+
+
+@tag("functional")
+class ValidateCGUTests(FunctionalTestCase):
+    def setUp(self):
+        self.aidant: Aidant = AidantFactory(
+            email="thierry@thierry.com",
+            validated_cgu_version=None,
+            post__with_otp_device=True,
+        )
+
+    def test_validate_cgu(self):
+        self.open_live_url(reverse("espace_aidant_cgu"))
+
+        # Login
+        self.login_aidant(self.aidant)
+
+        self.assertIsNone(self.aidant.validated_cgu_version)
+
+        # Espace Aidant home
+        self.selenium.find_element(By.CSS_SELECTOR, 'label[for="id_agree"]').click()
+        self.selenium.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+        self.wait.until(self.path_matches("espace_aidant_home"))
+
+        el = self.selenium.find_element(
+            By.XPATH, "//*[contains(@class, 'fr-alert') and contains(., 'CGU')]"
+        )
+        self.assertEqual(
+            "Merci d’avoir validé les CGU Aidants Connect.", el.text.strip()
+        )
+
+        self.aidant.refresh_from_db()
+        self.assertEqual(
+            settings.CGU_CURRENT_VERSION, self.aidant.validated_cgu_version
+        )
