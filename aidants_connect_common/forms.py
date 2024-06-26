8ce32ad7a83e2d25521942eddff769a0fc3011c4
Christophe Henry <contact@c-henry.fr>
2024-06-26 17:33:24 +0200

    Add BaseMultiForm and BaseModelMultiForm

--- aidants_connect_common/forms.py
+++ aidants_connect_common/forms.py
@@ -1,14 +1,26 @@
+from copy import deepcopy
 from datetime import timedelta
 from inspect import signature
+from itertools import accumulate
+from typing import Iterable, Union
 
 from django import forms
 from django.conf import settings
 from django.core import validators
-from django.core.exceptions import ValidationError
-from django.db.models import Model
-from django.forms import Form
+from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
+from django.db.models import Model, QuerySet
+from django.db.models.utils import AltersData
+from django.forms import (
+    BaseForm,
+    BaseFormSet,
+    BaseModelForm,
+    BaseModelFormSet,
+    Media,
+    MediaDefiningClass,
+)
 from django.forms.utils import ErrorList
 from django.utils.datastructures import MultiValueDict
+from django.utils.translation import ngettext
 
 from dsfr.forms import DsfrBaseForm
 from phonenumber_field.formfields import PhoneNumberField
@@ -53,7 +65,7 @@ class PatchedModelForm(forms.ModelForm, WidgetAttrMixin):
         super().__init__(*sig.args, **sig.kwargs)
 
 
-class PatchedForm(Form, WidgetAttrMixin):
+class PatchedForm(forms.Form, WidgetAttrMixin):
     def __init__(self, *args, **kwargs):
         sig = signature(super().__init__).bind_partial(*args, **kwargs)
         sig.arguments.setdefault("label_suffix", "")
@@ -142,3 +154,272 @@ class FollowMyHabilitationRequesrForm(DsfrBaseForm):
                 "Il nʼexiste pas de demande dʼhabilitation associée à cet email. "
                 "Veuillez vérifier votre saisie ou renseigner une autre adresse email."
             )
+
+
+FormLike = Union[BaseForm, BaseFormSet]
+ModelFormLike = Union[BaseModelForm, BaseModelFormSet]
+
+
+class DeclarativeFormMetaclass(MediaDefiningClass):
+    def __new__(mcs, name, bases, attrs):
+        attrs["declared_form_classes"] = {
+            key: attrs.pop(key)
+            for key, value in list(attrs.items())
+            if isinstance(value, type) and issubclass(value, FormLike)
+        }
+
+        new_class = super().__new__(mcs, name, bases, attrs)
+
+        # Walk through the MRO.
+        declared_form_classes = {}
+        for base in reversed(new_class.__mro__):
+            # Collect fields from base class.
+            if hasattr(base, "declared_form_classes"):
+                declared_form_classes.update(base.declared_form_classes)
+
+            # Field shadowing.
+            for attr, value in base.__dict__.items():
+                if value is None and attr in declared_form_classes:
+                    declared_form_classes.pop(attr)
+
+        new_class.base_form_casses = declared_form_classes
+
+        return new_class
+
+
+class BaseMultiForm(metaclass=DeclarativeFormMetaclass):
+    def __init__(
+        self,
+        data=None,
+        files=None,
+        auto_id="id_%s",
+        prefix=None,
+        initial=None,
+        error_class=ErrorList,
+        form_kwargs=None,
+        renderer=None,
+    ):
+        self.is_bound = data is not None or files is not None
+        self.prefix = prefix or self.get_default_prefix()
+        self.auto_id = auto_id
+        self.data = MultiValueDict() if data is None else data
+        self.files = MultiValueDict() if files is None else files
+        self.initial = initial or {}
+        self.error_class = error_class or ErrorList
+        self.form_kwargs = form_kwargs or {}
+        self.renderer = renderer
+        self._errors = None  # Stores the errors after clean() has been called.
+        self.form_classes = deepcopy(self.base_form_casses)
+
+        self._forms_cache = {}
+
+    def __repr__(self):
+        if self._errors is None:
+            is_valid = "Unknown"
+        else:
+            is_valid = self.is_bound and not self._errors
+        return (
+            f"<{self.__class__.__name__} "
+            f"bound={self.is_bound}, "
+            f"valid={is_valid}, "
+            f"form_classes={self.form_classes}>"
+        )
+
+    def __iter__(self) -> Iterable[FormLike]:
+        for name in self.forms:
+            yield self[name]
+
+    def __getitem__(self, name) -> FormLike:
+        try:
+            return self.forms[name]
+        except KeyError:
+            raise KeyError(
+                f"Key '{name}' not found in '{self.__class__.__name__}'. "
+                f"Choices are: {', '.join(sorted(self.form_classes.values()))}."
+            )
+
+    @property
+    def forms(self) -> dict[str, FormLike]:
+        if not self._forms_cache:
+            for name in self.form_classes:
+                self._forms_cache[name] = self.get_form(name)
+        return self._forms_cache
+
+    @classmethod
+    def get_default_prefix(cls):
+        return "multiform"
+
+    def get_form(self, name):
+        try:
+            form_class = self.form_classes[name]
+        except KeyError:
+            raise KeyError(
+                f"Key '{name}' not found in '{self.__class__.__name__}'. "
+                f"Choices are: {', '.join(sorted(self.form_classes.values()))}."
+            )
+        return self._construct_form(
+            name, form_class, **self.get_form_kwargs(name, form_class)
+        )
+
+    def get_form_kwargs(self, name, form_class):
+        return self.form_kwargs.get(name, {}).copy()
+
+    def _construct_form(self, name, form_class, **kwargs):
+        """Instantiate and return the i-th form instance in a formset."""
+        defaults = {
+            "auto_id": self.auto_id,
+            "prefix": self.add_prefix(name),
+            "error_class": self.error_class,
+            "renderer": self.renderer,
+        }
+        if self.is_bound:
+            defaults["data"] = self.data
+            defaults["files"] = self.files
+        if self.initial and (initial := self.initial.get(name)):
+            defaults["initial"] = initial
+
+        defaults.update(kwargs)
+
+        if issubclass(form_class, BaseFormSet):
+            # renderer is not a constructor argument of formsets
+            # It need to be processed differently
+            renderer = defaults.pop("renderer", None)
+            form = form_class(**defaults)
+            if renderer:
+                form.renderer = renderer
+            return form
+
+        return form_class(**defaults)
+
+    def add_non_field_error(self, form: str | None, error: str | ValidationError):
+        if not isinstance(error, ValidationError):
+            error = ValidationError(error)
+
+        if hasattr(error, "error_dict"):
+            raise TypeError(
+                "The `error` argument cannot contains errors for multiple fields."
+            )
+
+        form = form or NON_FIELD_ERRORS
+
+        if form in self.forms and isinstance(self.forms[form], BaseFormSet):
+            self.forms[form].non_form_errors().extend(error.error_list)
+            self.errors.setdefault(NON_FIELD_ERRORS, {})
+            self.errors[NON_FIELD_ERRORS].update(
+                {form: self.forms[form].non_form_errors()}
+            )
+        elif form in self.forms:
+            self.forms[form].add_error(None, error)
+            self.errors.setdefault(form, self.forms[form].errors)
+        elif form == NON_FIELD_ERRORS:
+            self.errors.setdefault(NON_FIELD_ERRORS, {})
+            self.errors[NON_FIELD_ERRORS].setdefault(
+                NON_FIELD_ERRORS,
+                self.error_class(error_class="nonfield", renderer=self.renderer),
+            )
+            self.errors[NON_FIELD_ERRORS][NON_FIELD_ERRORS].extend(error.error_list)
+        else:
+            raise ValueError(
+                "'form' argument must be a form name present in "
+                f"{self.form_classes.keys()} or None (was {form})"
+            )
+
+    def add_prefix(self, name):
+        return "%s-%s" % (self.prefix, name)
+
+    def is_valid(self):
+        return self.is_bound and not self.errors
+
+    @property
+    def errors(self) -> dict[str, ErrorList | dict[str, ErrorList]] | None:
+        """Return a list of form.errors for every form in self.forms."""
+        if self._errors is None:
+            self.full_clean()
+        return self._errors
+
+    def full_clean(self):
+        self._errors = {}
+
+        if not self.is_bound:  # Stop further processing.
+            return
+
+        for name, form in self.forms.items():
+            if form.is_valid():
+                if not hasattr(self, "cleaned_data"):
+                    self.cleaned_data = {}
+                self.cleaned_data[name] = form.cleaned_data
+            else:
+                if form.errors:
+                    self._errors[name] = form.errors
+                if isinstance(form, BaseFormSet) and form.non_form_errors():
+                    self.errors.setdefault(NON_FIELD_ERRORS, {})
+                    self.errors[NON_FIELD_ERRORS].update({name: form.non_form_errors()})
+
+        try:
+            self.clean()
+        except ValidationError as e:
+            self.add_non_field_error(None, e)
+
+    def clean(self):
+        pass
+
+    @property
+    def media(self):
+        return accumulate((form.media for form in self), initial=Media())
+
+
+class BaseModelMultiForm(BaseMultiForm, AltersData):
+    def __init__(
+        self,
+        data=None,
+        files=None,
+        auto_id="id_%s",
+        prefix=None,
+        querysets: None | dict[str, QuerySet] = None,
+        initial=None,
+        error_class=ErrorList,
+        form_kwargs=None,
+        renderer=None,
+    ):
+        self.querysets = querysets or {}
+        super().__init__(
+            data, files, auto_id, prefix, initial, error_class, form_kwargs, renderer
+        )
+
+    def get_queryset(self, name) -> QuerySet:
+        return self.querysets.get(name, None)
+
+    def get_form_kwargs(self, name, form_class):
+        kwargs = super().get_form_kwargs(name, form_class)
+        queryset = self.get_queryset(name)
+        if (
+            "queryset" not in kwargs
+            and queryset is not None
+            and issubclass(self.form_classes[name], ModelFormLike)
+        ):
+            kwargs["queryset"] = queryset
+        return kwargs
+
+    @property
+    def model_forms(self):
+        for form in self:
+            if isinstance(form, ModelFormLike):
+                yield form
+
+    def save(self, commit=True):
+        if self.errors:
+            raise ValueError(
+                ngettext(
+                    "The form %s could not be saved because it has errors.",
+                    "The forms %s could not be saved because it has errors.",
+                    len(self.errors),
+                )
+                % (
+                    list(self.errors.keys())[0]
+                    if len(self.errors) == 1
+                    else ", ".join(self.errors.keys())
+                ),
+            )
+
+        for form in self.model_forms:
+            form.save(commit)
