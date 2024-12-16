93830ce453350d5cb1d359ab245c2066edd1202e
MrJmad Aka Jean-Michel <j-mad@j-mad.com>
2024-12-16 09:43:53 +0100

    fix migration

--- aidants_connect_habilitation/migrations/0027_aidantrequest_habilitation_request.py
+++ aidants_connect_habilitation/migrations/0027_aidantrequest_habilitation_request.py
@@ -14,13 +14,13 @@ class Migration(migrations.Migration):
             name='habilitation_request',
             field=models.OneToOneField(default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='aidant_request', to='aidants_connect_web.habilitationrequest'),
         ),
-        migrations.RunSQL(
-            """
-            UPDATE aidants_connect_habilitation_aidantrequest ar
-            SET habilitation_request_id = hr.id
-            FROM aidants_connect_web_habilitationrequest hr
-            WHERE LOWER(ar.email) = LOWER(hr.email)
-            """,
-            elidable=True
-        )
+        # migrations.RunSQL(
+        #     """
+        #     UPDATE aidants_connect_habilitation_aidantrequest ar
+        #     SET habilitation_request_id = hr.id
+        #     FROM aidants_connect_web_habilitationrequest hr
+        #     WHERE LOWER(ar.email) = LOWER(hr.email)
+        #     """,
+        #     elidable=True
+        # )
     ]
